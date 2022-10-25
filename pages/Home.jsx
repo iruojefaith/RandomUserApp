ea806485161f8b45f77677d34b3c46718388c513
iruojefaith <fumsy240@gmail.com>
2022-10-25 01:23:34 +0100

    third commit

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,11 +4,21 @@ import { Link } from "react-router-dom";
 export default function App() {
   return (
     <>
-      <div className="home-page-container">
-        <span>Hi there, </span>
-        <p className="paragraph">This website consume a third party api to generate random multiple users, click the button below to check</p>
-        <Link to="/users">View Users Page</Link>
-      </div>
+    <div className="home-page-layout">
+
+        <div className="home-page-container-one">
+              <div className="span"></div>
+          </div>
+          <div className="home-page-container">
+                <span>Hi there, </span>
+                <p className="paragraph">This website consume a third party api to generate random multiple users, click the button below to check</p>
+                <Link to="/users"><button>View Users Page</button></Link>
+          </div>
+          <div className="home-page-container-two">
+
+          </div>
+    </div>
+
     </>
   );
 }
